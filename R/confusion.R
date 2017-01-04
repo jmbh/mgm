f11493debc22230096e83902eb495f5ede9c5c94
jmbh <jonashaslbeck@gmail.com>
2017-01-04 15:37:07 +0100

    adapt confusion, small stuff

--- R/confusion.R
+++ R/confusion.R
@@ -1,38 +1,82 @@
 
-confusion <- function(tg, eg) {
-  
 
-  #check on input
-  check <- apply(tg, 1:2, function(x) { x %in% c(0,1)})  
-  if(sum(check)!=length(tg)){
-    stop("Only zeros and ones allowed in the adjacency matrix!")
+confusion <- function(tg, # True Graph
+                      eg, # Estimated Graph
+                      undirected = TRUE, 
+                      diagonal = FALSE
+                      ) 
+  {
+  
+  
+  # ----- Input Checks -----
+  
+  # Square Matrix
+  if(nrow(tg)!=ncol(tg)) stop('True graph has to be represented by a square matrix.')
+  if(nrow(eg)!=ncol(eg)) stop('Estimated graph has to be represented by a square matrix.')
+  
+  # Zero-One
+  if(sum(tg %in% c(0,1)) != nrow(tg)^2) stop('The true graph has to be represented by a matrix containing only 0s and 1s.')
+  if(sum(eg %in% c(0,1)) != nrow(eg)^2) stop('The estimated graph has to be represented by a matrix containing only 0s and 1s.')
+  
+  
+  if(undirected) {
+    
+    # Symmetric
+    if(!isSymmetric(tg)) stop('True graph has to be symmetric. For directed graphs choose undirected=FALSE.')
+    if(!isSymmetric(eg)) stop('Estimated graph has to be symmetric. For directed graphs choose undirected=FALSE.')
+    
+  }
+  
+  # ----- Compute Measures -----
+  
+  if(undirected) {
+    
+    # Take upper triangular entries of both matrices
+    tg_utr <- tg[upper.tri(tg)]
+    eg_utr <- eg[upper.tri(eg)]
+    
+    sen <- mean(eg_utr[which(tg_utr == 1)] == 1) # Sensitivity
+    pre <- mean(tg_utr[which(eg_utr == 1)] == 1) # Precision
+    acc <- mean(eg_utr == tg_utr) # Accuracy
+    spe <- mean(eg_utr[which(tg_utr == 0)] == 0) # Specificity
+    
+    
+  } else {
+    
+    # Turn matrix into vector
+    if(diagonal) {
+      diag_tg <- diag(tg)
+      diag_eg <- diag(eg)
+    } else {
+      diag_tg <- NULL
+      diag_eg <- NULL
+    }
+    
+    tg_vec <- c(tg[upper.tri(tg)], tg[lower.tri(tg)], diag_tg)
+    eg_vec <- c(eg[upper.tri(eg)], eg[lower.tri(eg)], diag_eg)
+    
+    
+    sen <- mean(eg_vec[which(tg_vec == 1)] == 1) # Sensitivity
+    pre <- mean(tg_vec[which(eg_vec == 1)] == 1) # Precision
+    acc <- mean(eg_vec == tg_vec) # Accuracy
+    spe <- mean(eg_vec[which(tg_vec == 0)] == 0) # Specificity
+    
+    
   }
   
   
-  diag(tg) <- diag(eg) <- NA
-  m_eval <- cbind(tg[which(!is.na(tg))], eg[which(!is.na(eg))])
+  outlist <- list('sensitivity'= sen, 
+                  'precision' = pre, 
+                  'accuracy' = acc, 
+                  'specificity' = spe, 
+                  'call'=list('tg' = tg,
+                              'eg' = eg,
+                              'undirected' = undirected,
+                              'diagonal' = diagonal))
   
-  ev <- t(apply(m_eval, 1, function(x) {
-    acc <- x[1]==x[2]
-    if(x[1]==1 & x[2]==1) se<-1 else {se<-0}
-    if(x[1]==0 & x[2]==0) sp<-1 else {sp<-0}
-    return(cbind(acc, se, sp))
-  }))
+  return(outlist)
   
-  m_eval2 <- cbind(m_eval, ev)
   
-  #precision
-  m_eval_top <- m_eval[which(m_eval[,2]==1),] #m_eval - test outcome positive (top)
-  pre <- mean(m_eval_top[,1]==m_eval_top[,2]) #precision
   
-  a <- mean(m_eval2[,3]) #accuracy
-  se <- sum(m_eval2[,4])/sum(m_eval[,1]) #sensitivity
-  sp <- sum(m_eval2[,5])/(nrow(m_eval)-sum(m_eval[,1])) #specifity  
-  out.list <- list("accuracy"=a, "sensitivity"=se, "specificity"=sp, "precision"=pre)
-  
-  return(out.list)
-}
-
-
-
+} # eoF
 
