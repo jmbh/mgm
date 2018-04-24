5603e61779f2f341a0e9a4337323ab1f90c33706
jmbh <jonashaslbeck@gmail.com>
2018-04-24 12:34:50 +0200

    showInteraction() print; spell out the sign

--- R/print.int.R
+++ R/print.int.R
@@ -9,12 +9,17 @@ print.int <- function(x,
 {
   
   # Sign Defined?  
-  if(x$sign == 0) sign <- "Not Defined" else sign <- x$sign 
+  if(x$sign == 0) sign <- "NA" else sign <- x$sign 
+  
+  # Signtext
+  if(x$sign == 0) signtext <- " (Not defined)"
+  if(x$sign == 1) signtext <- " (Positive)"
+  if(x$sign == -1) signtext <- " (Negative)"
   
   # Create Console Output
   cat('Interaction:', paste(x$variables, collapse = "-"),
         '\nWeight: ', x$edgeweight,
-        '\nSign: ' , sign)
+        '\nSign: ' , sign, signtext)
 
 }
   
